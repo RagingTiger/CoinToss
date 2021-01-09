22a67e2367fb6cd9df4ef06730518b389bdec7e1
RagingTiger <RagingTiger@yourmom.com>
2021-01-09 16:28:09 -0500

    Complete overhaul to use fire library from Google

--- README.md
+++ README.md
@@ -10,116 +10,49 @@ pip install -r requirements.txt
 ```
 
 ## Usage
+The `command line interface` portion of this module uses the
+[fire](https://github.com/google/python-fire.git) library, which generates the
+below documentation by running `python cointoss.py` as follows:
 ```
-cointoss [-i] <games>
-cointoss [-i]
+NAME
+    cointoss.py - A class with various methods for simulating a coin toss.
+
+SYNOPSIS
+    cointoss.py COMMAND
+
+DESCRIPTION
+    A class with various methods for simulating a coin toss.
+
+COMMANDS
+    COMMAND is one of the following:
+
+     simulate
+       Run coin toss simulation with heads considered wins (i.e. 1) and tails
+       considered losses (i.e. 0).
 ```
-
-## Example
-### Basic: Instance Mode
-Running a single instance of 10 games (i.e. coin tosses or flips):
+To find out more about any of the `COMMANDS` shown above simply run the help
+flag `--help` after the command. For example to learn about the `simulate`
+command:
 ```
-$ python cointoss.py 10
-Heads(1) = 46
-Tails(0) = 54
-Total(1+0) = 100
-Percentage Heads = 46.0%
-Percentage Edge = -4.0%
+NAME
+    cointoss.py simulate - Run coin toss simulation with heads considered wins
+    (i.e. 1) and tails considered losses (i.e. 0).
+
+SYNOPSIS
+    cointoss.py simulate GAMES <flags>
+
+DESCRIPTION
+    Run coin toss simulation with heads considered wins (i.e. 1) and tails
+    considered losses (i.e. 0).
+
+POSITIONAL ARGUMENTS
+    GAMES
+        Number of coin toss games to play.
+
+FLAGS
+    --probability=PROBABILITY
+        Probability of coin toss resulting in heads.
+
+NOTES
+    You can also use flags syntax for POSITIONAL ARGUMENTS
 ```
-
-### Advanced: Interactive Mode
-Whereas the basic usage is only single instances with no saved state, the
-advanced usage drops into an interactive interpreter that saves state between
-sets of games. For example:
-```
-$ python cointoss.py -i
-> run 10
-Heads(1) = 5
-Tails(0) = 5
-Total(1+0) = 10
-Percentage Heads = 50.0%
-Percentage Edge = 0.0%
-
-> run 100
-Heads(1) = 38
-Tails(0) = 62
-Total(1+0) = 100
-Percentage Heads = 38.0%
-Percentage Edge = -12.0%
-
-> run 1000
-Heads(1) = 511
-Tails(0) = 489
-Total(1+0) = 1000
-Percentage Heads = 51.1%
-Percentage Edge = 1.1000000000000014%
-
-> run 10000
-Heads(1) = 5083
-Tails(0) = 4917
-Total(1+0) = 10000
-Percentage Heads = 50.83%
-Percentage Edge = 0.8299999999999983%
-
-> run 100000
-Heads(1) = 49882
-Tails(0) = 50118
-Total(1+0) = 100000
-Percentage Heads = 49.882%
-Percentage Edge = -0.1180000000000021%
-
-> run 10000000
-Heads(1) = 5000164
-Tails(0) = 4999836
-Total(1+0) = 10000000
-Percentage Heads = 50.00164%
-Percentage Edge = 0.0016400000000018622%
-```
-In the above example you can see we are increasing the number of games using
-the **interactive** command `run`, but there are a few other commands we can
-use while in the **interpreter**:
-```
-> help
-run [<games>] Runs the simulation with inputs
-total                Prints running total
-help                 Prints this help message
-
-> total
-Total Heads = 5055683
-Total Tails = 5055427
-Total Percentage Heads = 50.00126593420505%
-Total Percentage Edge = 0.0012659342050511668%
-
-> run
-Heads(1) = 5002020
-Tails(0) = 4997980
-Total(1+0) = 10000000
-Percentage Heads = 50.0202%
-Percentage Edge = 0.02020000000000266%
-
-> run
-Heads(1) = 4998799
-Tails(0) = 5001201
-Total(1+0) = 10000000
-Percentage Heads = 49.987989999999996%
-Percentage Edge = -0.012010000000003629%
-
-> run 10000000000000
-Heads(1) = 4999998400491
-Tails(0) = 5000001599509
-Total(1+0) = 10000000000000
-Percentage Heads = 49.99998400491%
-Percentage Edge = -1.599509000271837e-05%
-```
-
-While the `help` command should be self-explanatory, the other commands could
-benefit from some clarification:
-
-+ The `total` command just lets us
-  see the current running totals for heads, tails, the percentage of coin tosses
-  that are heads (overall) and precent edge (i.e. the difference in probability
-  of heads vs. tails).
-
-+ The `run` command can **optionally** except an argument (e.g. the number of
-  games of coin toss to be played). Without an argument it will repeat the same
-  simulations with the previous argument values for `<games>`.
--- cointoss.py
+++ cointoss.py
@@ -1,197 +1,54 @@
 #!/usr/bin/env python
 
-"""
-Description: Coin toss simulator for command line
-References:
-Usage:
-    cointoss [-i] <games>
-    cointoss (-i)
-"""
-
 # libs
-import sys
 import random
-import readline
-import termcolor
 import numpy
-
-# funcs
-def ctxt(txt, color='yellow'):
-    """Print out colored text to stdout."""
-    return termcolor.colored(txt, color)
-
-
-def warn(txt, exit=True):
-    """Print exit message and exit."""
-    if exit:
-        sys.exit(ctxt(txt, color='red'))
-    else:
-        print(ctxt(txt, color='red'))
-
 
 # classes
 class CoinToss(object):
     """A class with various methods for simulating a coin toss."""
-    def __init__(self, clargs):
-        # startup readline
-        readline.parse_and_bind("tab: complete")
-
-        # store clargs
-        self.clargs = clargs
-
-        # get user prompt
-        self.prompt = '> '
-
-        # get totals
-        self.record = {'heads': 0, 'tails': 0}
-
-        # command dictionary
-        self.commands = {'run': self.run,
-                         'total': self.total,
-                         'help': self.print_help
-                         }
 
     @staticmethod
-    def simulate(params):
+    def simulate(games, probability=0.5):
         """Run coin toss simulation with heads considered wins (i.e. 1) and
            tails considered losses (i.e. 0).
+
+        Args:
+            games (int): Number of coin toss games to play.
+            probability (float): Probability of coin toss resulting in heads.
+
+        Returns:
+            heads (float): Percent of coin tosses resulting in heads.
+            tails (float): Percent of coin tosses resulting in tails.
+            total (int): Total amount of games played.
+            probability (float): Probability of coin toss resulting in heads.
         """
         # init heads and tails counters (wins and losses respectively)
-        heads = 0
-        tails = 0
+        heads, tails = 0, 0
 
         # init random number generator
         rand_gen = numpy.random.default_rng()
 
         # get number of heads (i.e. 1s) with 1/2 probability
-        heads = rand_gen.binomial(params['games'], params['probability'])
+        heads = rand_gen.binomial(games, probability)
 
-        # calculate tails/tails by subtracting heads/heads from total
-        tails = params['games'] - heads
+        # calculate tails by subtracting heads from total games
+        tails = games - heads
 
         # return heads/tails
-        return {'heads': heads, 'tails': tails, 'games': params['games']}
-
-    def interpreter(self):
-        """Starts interactive session to store state of games."""
-        while True:
-            try:
-                cmd = input(self.prompt)
-                self.execute_cmd(cmd)
-            except EOFError:
-                warn('\nClosing interactive session')
-            except KeyboardInterrupt:
-                print('')
-
-    def parse_cmd(self, cmd):
-        """Parse commands for interactive mode."""
-        # first split
-        return cmd.split()
-
-    def execute_cmd(self, command):
-        """Execute commands passed in interactive mode."""
-        # first parse
-        cmd_list = self.parse_cmd(command)
-
-        # now execute
-        try:
-            self.commands[cmd_list[0]](cmd_list)
-        except KeyError:
-            warn('Unknown command {0}'.format(cmd_list[0]), exit=False)
-
-    def print_help(self, args):
-        """Prints help message."""
-        help_msg = ('run [<games>] Runs the simulation with inputs\n'
-                    'total                Prints running total\n'
-                    'help                 Prints this help message\n')
-
-        # print help
-        print(ctxt(help_msg, color='red'))
-
-    @staticmethod
-    def print_results(results):
-        # Basic formula for calculating percentage
-        percentage = (float(results['heads']) / results['games']) * 100
-
-        # Print statements #
-        print(ctxt("Heads(1) = " + str(results['heads'])))
-        print(ctxt("Tails(0) = " + str(results['tails'])))
-        print(ctxt("Total(1+0) = " + str(results['games'])))
-        print(ctxt("Percentage heads = " + str(percentage) + "%"))
-        print(ctxt("Percentage Edge = " + str(percentage - 50.00) + "%\n"))
-
-    def total(self, args):
-        """Calculates and prints running total."""
-        # calculate totals
-        heads = self.record['heads']
-        tails = self.record['tails']
-        percent_heads = (float(heads) / (heads + tails)) * 100
-        edge = percent_heads - 50.00
-
-        # get format string
-        total_string = (
-            'Total Heads = {0}\n'
-            'Total Tails = {1}\n'
-            'Total Percentage Heads = {2}%\n'
-            'Total Percentage Edge = {3}%\n'
-        ).format(heads, tails, percent_heads, edge)
-
-        # print totals
-        print(ctxt(total_string))
-
-    def run(self, cmd_list):
-        """Sets up the parameters for the simulation and executes it."""
-        # get parameters
-        try:
-            params = {'games': int(cmd_list[1]), 'probability': 0.5}
-
-            # now update clargs
-            self.clargs['<games>'] = params['games']
-
-        except IndexError:
-            if self.clargs['<games>']:
-                params = {
-                          'games': int(self.clargs['<games>']),
-                          'probability': 0.5
-                         }
-            else:
-                print('Usage Error: Type \'help\' for more details')
-                return
-
-        # execute simulation
-        results = self.simulate(params)
-
-        # print out results
-        self.print_results(results)
-
-        # update total
-        self.record['heads'] += results['heads']
-        self.record['tails'] += results['tails']
+        return {
+            'heads': heads / games,
+            'tails': tails / games,
+            'total': games,
+            'probability': probability
+        }
 
 
 # executable only
 if __name__ == '__main__':
 
     # libs
-    from docopt import docopt
+    import fire
 
-    # get args
-    args = docopt(__doc__)
-
-    # check interactive
-    if args['-i']:
-        # we need to store state
-        game = CoinToss(args)
-
-        # start interactive
-        game.interpreter()
-
-    else:
-        # parameters
-        params = {'games': int(args['<games>']), 'probability': 0.5}
-
-        # execute simulation
-        results = CoinToss.simulate(params)
-
-        # print out results
-        CoinToss.print_results(results)
+    # bang bang
+    fire.Fire(CoinToss)
